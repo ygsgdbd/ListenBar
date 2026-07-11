8ce000f3990149babbe8f5dc40c9b9d3f89511f1
Rainbow <ygsgdbd@163.com>
2026-07-11 14:52:02 +0800

    fix: align menu bar icon with app logo

--- ListenBar/Sources/App/ListenBarApp.swift
+++ ListenBar/Sources/App/ListenBarApp.swift
@@ -28,7 +28,9 @@ struct ListenBarApp: App {
         MenuBarExtra {
             MenuBarView(store: store, updaterController: updaterController)
         } label: {
-            Image(systemName: "network")
+            Image("MenuBarIcon")
+                .renderingMode(.template)
+                .accessibilityLabel("ListenBar")
         }
         .menuBarExtraStyle(.menu)
     }
